c49a2cc06eaf67414f6e6b6f7060d413d9fb4d80
Max Geller <mxgeller@gmail.com>
2020-08-31 14:23:18 -0700

    building out basic pages

--- src/app/features/clients/clients.module.ts
+++ src/app/features/clients/clients.module.ts
@@ -1,9 +1,11 @@
+import { SharedModule } from './../../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientDetailComponent } from './pages/client-detail/client-detail.component';
 import { ClientListComponent } from './pages/client-list/client-list.component';
 import { CampusesModule } from './modules/campuses/campuses.module';
 import { ClientsRoutes } from './clients.routes';
+import { CoreModule } from 'src/app/core/core.module';
 
 
 
@@ -16,6 +18,8 @@ import { ClientsRoutes } from './clients.routes';
     CommonModule,
     ClientsRoutes,
     CampusesModule,
+    SharedModule,
+    CoreModule
 
   ]
 })
--- src/app/features/projects/projects.module.ts
+++ src/app/features/projects/projects.module.ts
@@ -1,16 +1,23 @@
+import { SharedModule } from './../../shared/shared.module';
 import { ProjectsRoutes } from './projects.routes';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectListComponent } from './pages/project-list/project-list.component';
 import { ProjectDetailComponent } from './pages/project-detail/project-detail.component';
+import { CoreModule } from 'src/app/core/core.module';
 
 
 
 @NgModule({
-  declarations: [ProjectListComponent, ProjectDetailComponent],
+  declarations: [
+    ProjectListComponent,
+    ProjectDetailComponent
+  ],
   imports: [
     CommonModule,
-    ProjectsRoutes
+    SharedModule,
+    CoreModule,
+    ProjectsRoutes,
   ]
 })
 export class ProjectsModule { }
