024f38fdb881a32fe0a92bf4620ce1b138e09eb4
Max Geller <mxgeller@gmail.com>
2020-08-31 12:57:15 -0700

    configuring page layouts

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,25 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { SharedModule } from './../../shared/shared.module';
 import { CoreModule } from './../../core/core.module';
-import { FeedComponent } from './components/feed/feed.component';
 import { BrowseTagsComponent } from './components/browse-tags/browse-tags.component';
-import { BrowseCoursesComponent } from './components/browse-courses/browse-courses.component';
-import { ReviewsComponent } from './components/reviews/reviews.component';
 import { StatsComponent } from './components/stats/stats.component';
-import { CtaComponent } from './components/cta/cta.component';
 import { HomeComponent } from './home.component';
-import { NewsletterComponent } from './components/newsletter/newsletter.component';
 
 @NgModule({
     declarations: [
         HomeComponent,
-        FeedComponent,
         BrowseTagsComponent,
-        BrowseCoursesComponent,
-        ReviewsComponent,
         StatsComponent,
-        CtaComponent,
-        NewsletterComponent,
     ],
     imports: [
         CommonModule,
