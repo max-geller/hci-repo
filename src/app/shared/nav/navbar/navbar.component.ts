9fae7bdf74624670edc16bcf59689939c7c45811
Max Geller <mxgeller@gmail.com>
2020-08-31 23:31:08 -0700

    modern navbar fade, animation

--- src/app/shared/nav/navbar/navbar.component.ts
+++ src/app/shared/nav/navbar/navbar.component.ts
@@ -1,14 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {
+  animate,
+  AnimationEvent,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
+
+export type FadeState = 'visible' | 'hidden';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
+  animations: [
+    trigger('state', [
+      state(
+        'visible',
+        style({
+          opacity: '1'
+        })
+      ),
+      state(
+        'hidden',
+        style({
+          opacity: '0'
+        })
+      ),
+      transition('* => visible', [animate('500ms ease-out')]),
+      transition('visible => hidden', [animate('500ms ease-out')])
+    ])
+  ],
 })
-export class NavbarComponent implements OnInit {
+
+
+
+export class NavbarComponent {
+
+  state: FadeState;
+  // tslint:disable-next-line: variable-name
+  private _show: boolean;
+  get show() {
+    return this._show;
+  }
+  @Input()
+  set show(value: boolean) {
+    if (value) {
+      this._show = value;
+      this.state = 'visible';
+    } else {
+      this.state = 'hidden';
+    }
+  }
+
+  animationDone(event: AnimationEvent) {
+    if (event.fromState === 'visible' && event.toState === 'hidden') {
+      this._show = false;
+    }
+  }
+
+  public buttonName: any = 'Show';
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
+
+  toggleSearch(): void {
+    this.show = !this.show;
+  }
 }
