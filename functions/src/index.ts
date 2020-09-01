fa07533324c145144103e85210a4747d64dd8504
Max Geller <mxgeller@gmail.com>
2020-09-01 15:19:30 -0700

    configuring firebase, functions still throwing errors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,30 @@
 import * as functions from 'firebase-functions';
+import { https } from 'firebase-functions'
+import * as admin from 'firebase-admin';
+import * as algoliasearch from 'algoliasearch';
+admin.initializeApp();
+const env = functions.config();
+
+
+
+const client = algoliasearch(env.algolia.appid, env.algolia.apikey);
+const index = client.initIndex('project');
+
+exports.indexProject = functions.firestore
+    .document('projects/{projectId}')
+    .onCreate((snap, context) => {
+        const data = snap.data();
+        const objectId = snap.id;
+
+        return index.addObject({
+            objectId,
+            ...data
+        });
+    });
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
-// });
+// 
