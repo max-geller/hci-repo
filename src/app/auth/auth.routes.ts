93ea22579c24b76b5b2378ea0f9365fdde082a1a
Max Geller <mxgeller@gmail.com>
2020-09-03 14:24:11 -0700

    adding auth guard

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -9,6 +9,7 @@ import { NotificationsComponent } from './pages/notifications/notifications.comp
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { TagsComponent } from './pages/tags/tags.component';
+import { AuthGuard } from '../core/guards/auth.guard';
 
 const routes: Routes = [
     {
@@ -19,34 +20,42 @@ const routes: Routes = [
     {
         path: 'projects',
         loadChildren: () => import('./features/projects/projects.module').then(m => m.ProjectsModule),
+        canActivate: [AuthGuard]
     },
     {
         path: 'indices',
         loadChildren: () => import('./features/indices/indices.module').then(m => m.IndicesModule),
+        canActivate: [AuthGuard]
     },
     {
         path: 'clients',
         loadChildren: () => import('./features/clients/clients.module').then(m => m.ClientsModule),
+        canActivate: [AuthGuard]
     },
     {
         path: 'tasks',
         loadChildren: () => import('./features/tasks/tasks.module').then(m => m.TasksModule),
+        canActivate: [AuthGuard]
     },
     {
         path: 'contractors',
         loadChildren: () => import('./features/contractors/contractors.module').then(m => m.ContractorsModule),
+        canActivate: [AuthGuard]
     },
     {
         path: 'rates',
         loadChildren: () => import('./features/rates/rates.module').then(m => m.RatesModule),
+        canActivate: [AuthGuard]
     },
     {
         path: 'database',
         loadChildren: () => import('./features/database/database.module').then(m => m.DatabaseModule),
+        canActivate: [AuthGuard]
     },
     {
         path: 'admin',
         loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
+        canActivate: [AuthGuard]
     },
     {
         path: 'sessions',
@@ -70,20 +79,22 @@ const routes: Routes = [
     },
     {
         path: 'profile',
-        component: ProfileComponent
+        component: ProfileComponent,
+        canActivate: [AuthGuard]
     },
-    
     {
         path: 'terms',
         component: TermsComponent
     },
     {
         path: 'notifications',
-        component: NotificationsComponent
+        component: NotificationsComponent,
+        canActivate: [AuthGuard]
     },
     {
         path: 'favorites',
-        component: FavoritesComponent
+        component: FavoritesComponent,
+        canActivate: [AuthGuard]
     },
 
 ];
--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -4,35 +4,23 @@ import { map, tap, take } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { AuthService } from './../services/auth.service';
 
-import { User } from '../models/user.model';
-import { Role } from '../models/user.model';
-
 @Injectable({
   providedIn: 'root'
 })
 
 export class AuthGuard implements CanActivate {
-  constructor(private auth: AuthService, private router: Router) { }
+  constructor(private auth: AuthService, private router: Router) {
 
+  }
 
-  canActivate() {
-    if (this.auth.isLoggedIn()) {
-
-      if (this.auth.user$['roles'] === 'Client') {
-        this.router.navigate(['/client']);
-        console.log(this.auth.user$ + 'whoa man, yous a client!')
-      }
-
-      if (this.auth.user$['roles'] === 'Admin') {
-        this.router.navigate(['/admin']);
-      }
-
-
-      if (this.auth.user$['roles'] === 'Team') {
-        this.router.navigate(['/team']);
-      }
-      this.router.navigate(['/403']);
-      return false;
-    }
+  canActivate(next, state): Observable<boolean> {
+    return this.auth.user$.pipe(
+      take(1),
+      map(user => !!user),
+      tap(loggedIn => {
+        console.log('access denied');
+        this.router.navigate(['auth/sessions/login']);
+      })
+    );
   }
 }
