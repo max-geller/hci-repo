8891442a5a31e2bb0d173a37af86c22f17a18db6
Max Geller <mxgeller@gmail.com>
2020-09-05 18:31:23 -0700

    fixing nav issues

--- src/app/shared/components/layouts/auth-layout/auth-layout.component.ts
+++ src/app/shared/components/layouts/auth-layout/auth-layout.component.ts
@@ -10,6 +10,7 @@ import { Observable, of } from 'rxjs';
 import { DOCUMENT } from '@angular/common';
 import { AuthService } from './../../../../core/services/auth.service';
 import { NgxSpinnerService } from 'ngx-spinner';
+import algoliasearch from 'algoliasearch/lite';
 
 export type FadeState = 'visible' | 'hidden';
 
@@ -18,17 +19,63 @@ export type FadeState = 'visible' | 'hidden';
 @Component({
   selector: 'app-auth-layout',
   templateUrl: './auth-layout.component.html',
-
+  animations: [
+    trigger('state', [
+      state(
+        'visible',
+        style({
+          opacity: '1'
+        })
+      ),
+      state(
+        'hidden',
+        style({
+          opacity: '0'
+        })
+      ),
+      transition('* => visible', [animate('500ms ease-out')]),
+      transition('visible => hidden', [animate('500ms ease-out')])
+    ])
+  ],
 })
 
 export class AuthLayoutComponent implements OnInit {
 
-
+  projectSearchConfig = {
+    indexName: 'PROJECT',
+    searchClient: algoliasearch('8K7V4TARGD', 'f4c1889dd27c98bebcaf99eb1e3c5fc2'),
+    hitsPerPage: 8,
+  };
+  taskSearchConfig = {
+    indexName: 'TASK',
+    searchClient: algoliasearch('8K7V4TARGD', 'f4c1889dd27c98bebcaf99eb1e3c5fc2'),
+    hitsPerPage: 8,
+  };
 
 
 
   user$: Observable<firebase.User>;
+  state: FadeState;
   // tslint:disable-next-line: variable-name
+  private _show: boolean;
+  get show() {
+    return this._show;
+  }
+  @Input()
+  set show(value: boolean) {
+    if (value) {
+      this._show = value;
+      this.state = 'visible';
+    } else {
+      this.state = 'hidden';
+    }
+  }
+
+  animationDone(event: AnimationEvent): any {
+    if (event.fromState === 'visible' && event.toState === 'hidden') {
+      this._show = false;
+    }
+  }
 
 
 
@@ -51,5 +98,11 @@ export class AuthLayoutComponent implements OnInit {
     setTimeout(() => {
       this.spinner.hide();
     }, 3000);
+  }
+
+
+
+  toggleSearch(): void {
+    this.show = !this.show;
   }
 }
