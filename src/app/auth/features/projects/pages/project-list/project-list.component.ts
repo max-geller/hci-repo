d703c8132226dffb614bfe8c8c480c56da4fe643
Max Geller <mxgeller@gmail.com>
2020-09-15 01:30:44 -0700

    misc style updates

--- src/app/auth/features/projects/pages/project-list/project-list.component.ts
+++ src/app/auth/features/projects/pages/project-list/project-list.component.ts
@@ -3,6 +3,10 @@ import { Observable } from 'rxjs';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { ProjectInterface } from './../../../../../core/models/project.model';
 
+export interface TagInterface {
+  
+}
+
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
@@ -12,11 +16,12 @@ export class ProjectListComponent {
   projects$: Observable<any>;
   projectDoc: AngularFirestoreDocument<ProjectInterface>;
   project: Observable<ProjectInterface>;
-
+tags$: Observable<TagInterface>
   constructor(afs: AngularFirestore) {
     this.projects$ = afs.collection('projects', ref => ref.orderBy('id')).valueChanges();
     this.projectDoc = afs.doc<ProjectInterface>('projects/${id}');
-    this.project = this.projectDoc.valueChanges();
+    this.tags$ = this.projectDoc.valueChanges();
+    console.log(this.project);
 
   }
 }
