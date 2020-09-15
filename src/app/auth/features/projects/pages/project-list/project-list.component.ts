8f2c9fff66a3ecd3c9f609e883c52eac8f593494
Max Geller <mxgeller@gmail.com>
2020-09-14 20:00:04 -0700

    attempting tags feature

--- src/app/auth/features/projects/pages/project-list/project-list.component.ts
+++ src/app/auth/features/projects/pages/project-list/project-list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
-import { AngularFirestore } from '@angular/fire/firestore';
-
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { ProjectInterface } from './../../../../../core/models/project.model';
 
 @Component({
   selector: 'app-project-list',
@@ -10,8 +10,13 @@ import { AngularFirestore } from '@angular/fire/firestore';
 
 export class ProjectListComponent {
   projects$: Observable<any>;
+  projectDoc: AngularFirestoreDocument<ProjectInterface>;
+  project: Observable<ProjectInterface>;
+
   constructor(afs: AngularFirestore) {
     this.projects$ = afs.collection('projects', ref => ref.orderBy('id')).valueChanges();
+    this.projectDoc = afs.doc<ProjectInterface>('projects/${id}');
+    this.project = this.projectDoc.valueChanges();
 
   }
 }
