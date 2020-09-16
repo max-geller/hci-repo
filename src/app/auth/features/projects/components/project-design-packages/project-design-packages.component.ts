22aa141c8277fa18d5dce933ffd3e529ff4325a3
Max Geller <mxgeller@gmail.com>
2020-09-15 20:46:24 -0700

    adding packages list to project detail page

--- src/app/auth/features/projects/components/project-design-packages/project-design-packages.component.ts
+++ src/app/auth/features/projects/components/project-design-packages/project-design-packages.component.ts
@@ -1,16 +1,66 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { Observable } from 'rxjs/internal/Observable';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AngularFirestore, AngularFirestoreCollectionGroup } from '@angular/fire/firestore';
+
+
+import { FirestoreService } from 'src/app/core/services/firestore.service';
+import { PackageInterface } from '../../../../../core/models/package.model';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-project-design-packages',
   templateUrl: './project-design-packages.component.html',
-  styleUrls: ['./project-design-packages.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProjectDesignPackagesComponent implements OnInit {
+  packages$: Observable<any>;
+  packages: MatTableDataSource<any>;
+  displayedColumns = [
+    'description',
+    'structure',
+    'squareFeet',
+    'type',
+    'storiesAboveGrade',
+  ];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
 
-  constructor() { }
+  constructor(private dataApi: FirestoreService, private route: ActivatedRoute, public afs: AngularFirestore) {
 
-  ngOnInit(): void {
   }
 
+  public project: PackageInterface = {
+    id: null,
+  };
+
+  ngOnInit(): any {
+    
+    const idProject = this.route.snapshot.params['id'];
+    this.getDetails(idProject);
+    console.log(idProject);
+    this.afs.collectionGroup('packages', ref => ref.where('projectId', '==', idProject)).valueChanges()
+    .subscribe(data => {
+      // 1e: ...and Pass that Data to the Material Data Table
+      this.packages = new MatTableDataSource(data);
+      this.packages.sort = this.sort;
+      this.packages.paginator = this.paginator;
+      this.packages.sort = this.sort;
+    });
+    console.log('heres the project:' + idProject);
+  }
+
+  getDetails(idProject: string): void {
+    this.dataApi.getOneProject(idProject).subscribe(project => {
+      this.project = project;
+
+    });
+  }
+  applyFilter(filterValue: string): void {
+    filterValue = filterValue.trim();
+    filterValue = filterValue.toLowerCase();
+    this.packages.filter = filterValue;
+  }
+  
 }
